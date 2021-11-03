ff647d8f2b128c7a224a96f7f0e9e927d8e6866f
say-bay@mail.ru <say-bay@mail.ru>
2021-11-03 17:44:32 +0700

    Добавьте файлы проекта.

---
+++ Lab16/Task1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace Task1
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            const int n = 5;
+            string jsonString = "";
+            Goods[] array = new Goods[n];
+            for (int i = 0; i < n; i++)
+            {
+                Goods newGoods = new Goods();
+                newGoods.readParam();
+                array[i] = newGoods;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                jsonString = jsonString + JsonSerializer.Serialize(array[i]);
+            }
+            FileInfo fi = new FileInfo("Products.json");
+            fi.Create();
+            StreamWriter sw = new StreamWriter("Products.json", false);
+            sw.WriteLine(jsonString);
+        }
+    }
+}
+class Goods
+{
+    public int Code { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public void readParam()
+    {
+        Console.WriteLine("Введите параметры товара");
+        Console.Write("Код: ");
+        int code = Convert.ToInt32(Console.ReadLine());
+        Code = code;
+        Console.Write("Наименование: ");
+        string name = Convert.ToString(Console.ReadLine());
+        Name = name;
+        Console.Write("Цена: ");
+        double price = Convert.ToDouble(Console.ReadLine());
+        Price = price;
+    }
+}
+
---
+++ Task2/Task2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace Task2
+{
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+            //string nGood = JsonSerializer.Serialize(sr.ReadLine());
+            //Console.WriteLine(nGood);
+            Goods[] texGoods = new Goods[5];
+            StreamReader sr = new StreamReader("Products.json");
+            for (int i = 0; i < 5; i++)
+            {
+                string txt = sr.ReadLine();
+                Goods newGoods = JsonSerializer.Deserialize<Goods>(txt);
+                texGoods[i] = newGoods;
+            }
+            sr.Close();
+            Console.ReadKey();
+        }
+
+
+        class Goods
+        {
+            public int Code { get; set; }
+            public string Name { get; set; }
+            public double Price { get; set; }
+
+        }
+    }
+}
